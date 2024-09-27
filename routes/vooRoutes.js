7c703e8c670678ca8fd0f0320baf35bca3be0678
Rodrigo Zanetti <116608232+RodrigoZanetti175@users.noreply.github.com>
2024-09-26 21:58:06 -0300

    Novo

--- routes/vooRoutes.js
+++ routes/vooRoutes.js
@@ -4,6 +4,7 @@ const Voo = require('../models/voo.js');
 
 router.use(function(req, res, next){
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 })
